1ef701171cba2626259a0f5bee2984c42ae6e64a
gireeshvpai2007-maker <gireeshvpai2007@gmail.com>
2026-07-18 11:59:27 +0530

    Add binary tree traversals

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<vector>
+#include<queue>
+#include<algorithm>
 using namespace std;
 
 class Node
@@ -9,7 +11,7 @@ public:
     Node* left;
     Node* right;
 
-    static int i;
+    
 
     Node(int val)
     {
@@ -18,8 +20,11 @@ public:
         right = NULL;
     }
 
+    
+};
+    static int i = 0;
     Node* buildTree(const vector<int>& v)
-    {
+    {    
         if(i >= v.size() || v[i] == -1)
         {
             i++;
@@ -33,20 +38,68 @@ public:
 
         return root;
     }
-};
 
-int Node::i = 0;
-
+void preorder(Node* root)
+{
+    if(root==NULL)
+    {
+       return ;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(Node* root)
+{
+    if(root==NULL)
+    {
+       return ;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+void postorder(Node* root)
+{
+    if(root==NULL)
+    {
+       return ;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+void levelorder(Node* root)
+{
+   
+    std::queue<Node*> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        Node* temp = q.front();
+        cout<<temp->data<<" ";
+        q.pop();
+        if(temp->left!=NULL)
+        {
+            q.push(temp->left);
+        }
+        if(temp->right!=NULL)
+        {
+            q.push(temp->right);
+        }
+}
+cout<<endl;
+}
 int main()
 {
-    vector<int> v = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
-
-    Node helper(0);
-
-    Node* root = helper.buildTree(v);
-
-    cout << root->data << endl;
-    cout << root->left->data << endl;
-    cout << root->right->data << endl;
-    cout << root->right->left->data << endl;
+    vector<int> v = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};  
+    Node * root = buildTree(v);
+  
+    preorder(root);
+    cout<<endl;
+    inorder(root);
+    cout<<endl;
+    postorder(root);
+    cout<<endl;
+    levelorder(root);
 }
