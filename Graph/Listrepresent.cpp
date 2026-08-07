cdc30f24566f2c5d4b31760c395f8daaa5f6fd75
gireeshvpai2007-maker <gireeshvpai2007@gmail.com>
2026-08-07 22:30:27 +0530

    Created a new folder to start Graph learning journet

---
+++ Graph/Listrepresent.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    cout<<"enter the no of vertices in the graph : ";
+    int n;
+    cin>>n;
+    vector<vector<int>>adj(n+1);
+    cout<<"enter the no of edges in the graph : ";
+    int m;  
+    cin>>m;
+    for(int i=0;i<m;i++)
+    {
+        cout<<"enter the edge : ";
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    cout<<"The adjacency list of the graph is : "<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i<<" -> ";
+        for(int j=0;j<adj[i].size();j++)
+        {
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
